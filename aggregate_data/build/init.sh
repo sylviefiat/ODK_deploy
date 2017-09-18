d4b0ea82fc06242eace067f32627d57250203f9b
Sylvie Fiat <sylvie.fiat@ird.fr>
2017-09-18 16:11:48 +1100

    changement build pour docker-compose build

---
+++ aggregate_data/build/init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /tmp
+#wget https://opendatakit.org/download/4456/
+#mv index.html ODK.run
+#chmod +x ODK.run
+
+export COMMANDS='\n\n\n\n\n\n\n\n\ny\n'${AGG_PATH}'\n3\nn\nn\n1\nn\n'${AGG_HTTP}'\n'${AGG_IP}'\nn\n'${AGG_DB_PORT}'\n'${AGG_CONTAINER_DB}'\n'${OGG_DB_USER}'\n'${AGG_DB_PWD}'\n'${AGG_DB_PWD}'\n'${AGG_DB_NAME}'\n'${AGG_DB_SCHEMA}'\n'${AGG_PROJECT}'\n'${AGG_USERNAME}'\nY\nY\n'
+echo $COMMANDS
+
+#printf '\n\n\n\n\n\n\n\n\ny\n/tmp/aggregate\n3\nn\nn\n1\nn\n8080\nhttp://10.41.2.232\nn\n\naggregatedb\n\ndocker\ndocker\n\n\naggregate\nadmin\nY\nY\n' | ./ODK.run
+printf $COMMANDS | ./ODK.run
+find -name "*\ *" -type d | rename 's/ /_/g' && find / -name aggregate
+mv /tmp/aggregate/ODK_Aggregate/ODKAggregate.war /usr/local/tomcat/webapps/
+cp /tmp/aggregate/ODK_Aggregate/create_db_and_user.sql /sql/
+
--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-NAME=$(basename $PWD | awk '{print tolower($0)}')
-
-# create the build image
-docker build -t ${NAME} -f Dockerfile.build $PWD
-
