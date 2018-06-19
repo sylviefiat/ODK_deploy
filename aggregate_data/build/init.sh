35f9d8c57e2d66ae2a1ebf3ba4a3da6c91ef0637
Fiat <sylvie.fiat@ird.fr>
2018-06-19 13:18:32 +1100

    fix(typo): AGG_USER_DB

--- aggregate_data/build/init.sh
+++ aggregate_data/build/init.sh
@@ -5,7 +5,7 @@ wget https://opendatakit.org/download/4456/
 mv index.html ODK.run
 chmod +x ODK.run
 
-export COMMANDS='\n\n\n\n\n\n\n\n\ny\n'${AGG_PATH}'\n3\nn\nn\n1\nn\n'${AGG_HTTP}'\n'${AGG_IP}'\nn\n'${AGG_DB_PORT}'\n'${AGG_CONTAINER_DB}'\n'${OGG_DB_USER}'\n'${AGG_DB_PWD}'\n'${AGG_DB_PWD}'\n'${AGG_DB_NAME}'\n'${AGG_DB_SCHEMA}'\n'${AGG_PROJECT}'\n'${AGG_USERNAME}'\nY\nY\n'
+export COMMANDS='\n\n\n\n\n\n\n\n\ny\n'${AGG_PATH}'\n3\nn\nn\n1\nn\n'${AGG_HTTP}'\n'${AGG_IP}'\nn\n'${AGG_DB_PORT}'\n'${AGG_CONTAINER_DB}'\n'${AGG_DB_USER}'\n'${AGG_DB_PWD}'\n'${AGG_DB_PWD}'\n'${AGG_DB_NAME}'\n'${AGG_DB_SCHEMA}'\n'${AGG_PROJECT}'\n'${AGG_USERNAME}'\nY\nY\n'
 echo $COMMANDS
 
 #printf '\n\n\n\n\n\n\n\n\ny\n/tmp/aggregate\n3\nn\nn\n1\nn\n8080\nhttp://10.41.2.232\nn\n\naggregatedb\n\ndocker\ndocker\n\n\naggregate\nadmin\nY\nY\n' | ./ODK.run
